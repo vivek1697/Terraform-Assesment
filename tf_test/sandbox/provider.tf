a6de6ee33e47df832acf878bb2990b6776e7f5e7
Vivek Patel <vivek.d.ptl@gmail.com>
2022-10-23 22:02:41 -0400

    Added aws credentials path in the provider.tf

--- tf_test/sandbox/provider.tf
+++ tf_test/sandbox/provider.tf
@@ -3,8 +3,9 @@ terraform {
 }
 
 provider "aws" {
-  region              = var.aws_region
-  allowed_account_ids = var.aws_account_ids
-  profile             = "default"
-  version             = ">= 2.46.0"
+  region                  = var.aws_region
+  shared_credentials_file = "~/.aws/credentials"
+  allowed_account_ids     = var.aws_account_ids
+  profile                 = "default"
+  version                 = ">= 2.46.0"
 }
