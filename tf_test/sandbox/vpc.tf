df4ef693319d00c7f4d8a9f547153f35d076240e
Vivek Patel <vivek.d.ptl@gmail.com>
2022-10-23 20:57:04 -0400

    Added region and private subnet in the vpc.tf file

--- tf_test/sandbox/vpc.tf
+++ tf_test/sandbox/vpc.tf
@@ -1,10 +1,12 @@
 module "sandbox_vpc" {
   source = "../modules/vpc"
 
-  availability_zones        = var.availability_zones
-  environment               = var.environment
-  vpc_cidr_block            = "172.33.0.0/16"
-  public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
+  aws_region                  = var.aws_region
+  availability_zones          = var.availability_zones
+  environment                 = var.environment
+  vpc_cidr_block              = "172.33.0.0/16"
+  public_subnet_cidr_blocks   = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks  = var.private_subnet_cidr_blocks
 }
 
 
