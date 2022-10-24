814440c090335a9fdc161d2c7344c09a6331e7de
Vivek Patel <vivek.d.ptl@gmail.com>
2022-10-23 20:18:47 -0400

    Created separate variables.tf file, fixed private subnet cidar block type from string to list

--- tf_test/sandbox/vpc.tf
+++ tf_test/sandbox/vpc.tf
@@ -1,35 +1,3 @@
-variable "public_subnet_cidr_blocks" {
-  type = list
-  default = [
-    "172.33.10.0/24",
-    "172.33.20.0/24",
-    "172.33.30.0/24",
-  ]
-}
-
-variable "private_subnet_cidr_blocks" {
-  type = string
-  default = [
-    "172.33.100.0/24",
-    "172.33.110.0/24",
-    "172.33.120.0/24",
-  ]
-}
-
-variable "availability_zones" {
-  type = list
-  default = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c",
-  ]
-}
-
-variable "environment" {
-  type    = string
-  default = null
-}
-
 module "sandbox_vpc" {
   source = "../modules/vpc"
 
